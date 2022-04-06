015158af678a721667250446e6ae39edc7b4d557
Wen An Thian <wenan.thian@cognizant.com>
2022-04-06 23:44:18 +0800

    update db scripts and mysql yum repo rpm

--- db/dev.sql
+++ db/dev.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS app;
 
 USE app;
 
-CREATE TABLE
+CREATE TABLE IF NOT EXISTS
     users (
         id INT(11) NOT NULL auto_increment,
         name VARCHAR(100) NOT NULL,
